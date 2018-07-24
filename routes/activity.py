fba7af7328805968e3ca423aa109899e79d9fd35
Syed-Bakhtiyar <syedbakhtiyar120@gmail.com>
2018-07-25 02:22:41 +0500

    bug fix in activity

--- routes/activity.py
+++ routes/activity.py
@@ -1,9 +1,9 @@
 from flask import request, json
-from database_connection import connection
+from services.activity import creatrOurActivity
 
 def ourActivity(app):
     @app.route('/createouractivity', methods=['POST', 'GET'])
-    def createOurActivity():
+    def addOurActivity():
         if request.method == 'POST':
             shop_id = request.form['shop_id']
             product_type_id = request.form['product_type_id']
@@ -11,7 +11,7 @@ def ourActivity(app):
             comments = request.form['comments']
             image = request.form['image']
             date_today = request.form['date_today']
-            connection.creatrOurActivity(shop_id, product_type_id, product_sub_type_id, comments, image, date_today)
+            creatrOurActivity(shop_id, product_type_id, product_sub_type_id, comments, image, date_today)
             return shop_id
         else:
             return "hello"
