64f2e4bafd8cec71c2b2ea21ecf7be929afd8715
Syed-Bakhtiyar <syedbakhtiyar120@gmail.com>
2018-10-20 01:46:28 +0500

    company creation and read

--- schemas/company.py
+++ schemas/company.py
@@ -2,6 +2,7 @@ sqlCreateCompanyTable = "CREATE TABLE IF NOT EXISTS company (" \
                                      "ID INT NOT NULL AUTO_INCREMENT," \
                                      "ADMIN_ID INT NOT NULL," \
                                      "NAME VARCHAR(255) NOT NULL, " \
+                                    #  "UNIQUE (ADMIN_ID, NAME),"\
                                      "DATE_TODAY TIMESTAMP DEFAULT CURRENT_TIMESTAMP," \
                                      "PRIMARY KEY (ID), "\
                                      "FOREIGN KEY (ADMIN_ID) REFERENCES user(ID))"
